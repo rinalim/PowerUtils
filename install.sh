e4e527f553ac39390b440c6b5a3b6ca89026aeec
rinalim <46272802+rinalim@users.noreply.github.com>
2020-11-26 18:48:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
-sudo chmod 755 /home/pi/Battery/omxiv-battery
-sudo apt install python-smbus
+sudo apt install python-smbus -y
+
+rm -rf /opt/retropie/configs/all/PowerUtils/
+mkdir /opt/retropie/configs/all/PowerUtils/
+cp -f -r ./PowerUtils /opt/retropie/configs/all/
+
+sudo chmod 755 /opt/retropie/configs/all/PowerUtils/omxiv-battery
 sudo sed -i '/Battery.py/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\/usr/bin/python /home/pi/Battery/Battery.py &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\/usr/bin/python  /opt/retropie/configs/all/PowerUtils/Battery.py &' /opt/retropie/configs/all/autostart.sh
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."
