1927f20c546b3a611e58f0c3610663326b4becc3
rinalim <46272802+rinalim@users.noreply.github.com>
2021-01-01 22:11:58 +0900

    Update install-battery-power.sh

--- install-battery-power.sh
+++ install-battery-power.sh
@@ -1,4 +1,4 @@
-sudo apt install python-smbus -y
+sudo apt install python3-smbus -y
 
 rm -rf /opt/retropie/configs/all/PowerUtils/
 mkdir /opt/retropie/configs/all/PowerUtils/
@@ -7,7 +7,7 @@ cp -f -r ./PowerUtils /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/PowerUtils/omxiv-battery
 sudo sed -i '/Battery.py/d' /opt/retropie/configs/all/autostart.sh
 sudo sed -i '/PowerUtils/d' /opt/retropie/configs/all/autostart.sh
-sudo sed -i '1i\\sudo /usr/bin/python  /opt/retropie/configs/all/PowerUtils/BatteryPower.py &' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\sudo /usr/bin/python3  /opt/retropie/configs/all/PowerUtils/BatteryPower.py &' /opt/retropie/configs/all/autostart.sh
 
 echo
 echo "Setup Completed. Reboot after 3 Seconds."
