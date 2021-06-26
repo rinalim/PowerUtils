77a82da77edf826ccdf289ace86d5145c773ff74
rinalim <46272802+rinalim@users.noreply.github.com>
2021-06-27 02:11:05 +0900

    Create install-ina219.sh

---
+++ install-ina219.sh
@@ -0,0 +1,14 @@
+sudo apt install python3-smbus -y
+
+rm -rf /opt/retropie/configs/all/PowerUtils/
+mkdir /opt/retropie/configs/all/PowerUtils/
+cp -f -r ./PowerUtils /opt/retropie/configs/all/
+
+sudo chmod 755 /opt/retropie/configs/all/PowerUtils/omxiv-battery
+sudo sed -i '/Battery.py/d' /opt/retropie/configs/all/autostart.sh
+sudo sed -i '1i\\/usr/bin/python3 /opt/retropie/configs/all/PowerUtils/INA219.py &' /opt/retropie/configs/all/autostart.sh
+
+echo
+echo "Setup Completed. Reboot after 3 Seconds."
+sleep 3
+reboot
